6e87d501dc1e0256ab89dfe8d00efba8a1e1c9ff
Carlos Hernández <diamante.supremo.10000@gmail.com>
2025-12-17 08:32:48 -0600

    Update video URL for 'Increible Atardecer'

--- schema.sql
+++ schema.sql
@@ -13,4 +13,4 @@ CREATE TABLE videos (
 INSERT INTO videos (title, thumbnail, url, views) VALUES 
 ('Paisaje Relajante 4K', 'IMG-20251216-WA0001.jpg', 'grok_video_2025-12-17-08-25-34.mp4', 12050),
 ('Tutorial de Python en 5 min', 'IMG-20251216-WA0006.jpg', 'grok_video_2025-12-17-08-26-19.mp4', 5340),
-('Increible Atardecer', 'IMG-20251216-WA0008.jpg', 'https://www.w3schools.com/html/mov_bbb.mp4', 8900);
+('Increible Atardecer', 'IMG-20251216-WA0008.jpg', 'grok_video_2025-12-17-08-27-11.mp4', 8900);
